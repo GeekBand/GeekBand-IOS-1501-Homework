a4ab1f4436513c8d9c46e5cc3729e1f7de70aa87
sunzzww <sunzzww@icloud.com>
2015-08-10 22:57:54 +0800

    初始化项目

---
+++ G2015010480/HomeworkForOC/HomeworkForOC/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  HomeworkForOC
+//
+//  Created by 孙志伟 on 15/8/10.
+//  Copyright (c) 2015年 孙志伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    return 0;
+}
