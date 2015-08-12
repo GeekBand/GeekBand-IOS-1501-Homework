c23cc5ca91f32deef1b8b5da4623aad61a6daa43
lukellx <llx@MacBook-Pro.local>
2015-08-12 18:12:20 +0800

    G2015010103提交作业
    G2015010103提交作业

---
+++ G2015010103/Fish_Bird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fish_Bird
+//
+//  Created by 李凌霄 on 15/8/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ G2015010103/Fish_Bird/HWFish.h
@@ -0,0 +1,18 @@
+//
+//  HWFish.h
+//  G2015010103_Homework
+//
+//  Created by 李凌霄 on 15/8/12.
+//
+//
+
+#import "HWAnimal.h"
+#import <UIKit/UIKit.h>
+
+@interface HWFish : HWAnimal
+
+@property (nonatomic, strong) UIColor *color;
+
+- (void)swim;
+
+@end
