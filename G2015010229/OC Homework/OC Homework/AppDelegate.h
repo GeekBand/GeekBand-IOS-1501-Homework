7cf146eb90582d0a91e9fec40a2d336b015d84e6
BluesZhang <blues-zhang@qq.com>
2015-08-16 23:34:51 +0800

    张弢的OC作业

---
+++ G2015010229/OC Homework/OC Homework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC Homework
+//
+//  Created by 张弢 on 15/8/16.
+//  Copyright (c) 2015年 GeekBand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
