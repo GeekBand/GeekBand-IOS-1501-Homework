eba87660cb1aa48aeae9bb201f37cbfa1642ad7c
mnmhouse <312421794@qq.com>
2015-08-20 11:44:15 +0800

    项目作业
    蓦然的基本框架

---
+++ G2015010436/iOS_moran_project/GKMoranHouse/GKMoranHouse/Classes/Controllers/MyInfoVc.h
@@ -0,0 +1,13 @@
+//
+//  MyInfoVc.h
+//  GKMoranHouse
+//
+//  Created by house on 15/8/20.
+//  Copyright © 2015年 putact. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyInfoVc : UITableViewController
+
+@end
