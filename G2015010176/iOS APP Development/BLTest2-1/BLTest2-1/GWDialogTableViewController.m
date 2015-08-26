33926d8d7c2bb8d948dd9bdf25ed814a5a9634e7
Will Ge <Will@Wills-MacBook-Pro.local>
2015-08-26 19:20:36 +0800

    Update Test 2

---
+++ G2015010176/iOS APP Development/BLTest2-1/BLTest2-1/GWDialogTableViewController.m
@@ -0,0 +1,104 @@
+//
+//  GWDialogTableViewController.m
+//  BLTest2-1
+//
+//  Created by Will Ge on 8/26/15.
+//  Copyright (c) 2015 gewill.org. All rights reserved.
+//
+
+#import "GWDialogTableViewController.h"
+
+@interface GWDialogTableViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+@implementation GWDialogTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    // Return the number of rows in the section.
+    return 5;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *myCell = [tableView dequeueReusableCellWithIdentifier:@"myMessageCellIdentifier"];
+    
+    UITableViewCell *friendsCell = [tableView dequeueReusableCellWithIdentifier:@"friendsMessageCellIdentifier"];
+    
+    // Configure the cell...
+    
+    if (indexPath.row % 2 == 0) {
+        return myCell;
+    } else {
+        return friendsCell;
+    }
+
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
