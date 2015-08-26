0964005691c94b80ccd3d88aeaa478f7e8db6b38
Will Ge <Will@Wills-MacBook-Pro.local>
2015-08-26 17:17:39 +0800

    update BLTest2-2

--- G2015010318/Objective_C_Homework/build/Objective_C_Homework.build/Debug-iphonesimulator/Objective_C_Homework.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homework/Objective_C_Homework/AppDelegate.m \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homework/Objective_C_Homework/AppDelegate.h \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homework/Objective_C_Homework/HMBViewController.h
--- G2015010318/Objective_C_Homework/build/Objective_C_Homework.build/Debug-iphonesimulator/Objective_C_Homework.build/Objects-normal/x86_64/HMBGameObjectView.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homeworka/Objective_C_Homework/HMBGameObjectView.m \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homeworka/Objective_C_Homework/HMBGameObjectView.h \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homeworka/Objective_C_Homework/HMBBird.h \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homeworka/Objective_C_Homework/HMBAnimal.h \
-  /Users/Myron/Documents/IOS\ Test/GeekBand/Objective_C_Homeworka/Objective_C_Homework/HMBFish.h
