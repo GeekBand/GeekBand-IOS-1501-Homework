bb164379fc57892c74f5f05eeed55ba96f2a2ec8
Will Ge <Will@Wills-MacBook-Pro.local>
2015-08-31 20:15:30 +0800

    Add test 3

---
+++ G2015010176/极客班iOS应用开发实战测试题二（8月31日）/Classmates/Classmates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Classmates
+//
+//  Created by Will Ge on 8/31/15.
+//  Copyright (c) 2015 gewill.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
