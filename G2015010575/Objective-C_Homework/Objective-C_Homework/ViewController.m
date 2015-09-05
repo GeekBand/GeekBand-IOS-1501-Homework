97806678952cb1f7765cae07ca5098ab424bdedb
Martin.Zhou <zhking945@mail.ru>
2015-09-05 16:12:20 +0800

    Initial Commit

---
+++ G2015010575/Objective-C_Homework/Objective-C_Homework/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Objective-C_Homework
+//
+//  Created by 祝海焜 on 15/9/5.
+//  Copyright (c) 2015年 祝海焜. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
