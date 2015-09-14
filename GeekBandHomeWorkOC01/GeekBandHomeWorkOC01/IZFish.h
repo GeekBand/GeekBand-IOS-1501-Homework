99a943ec7a59e5813a7416936e036c69e6bc3e6b
BlueMonkey <i_lzsh@163.com>
2015-09-15 00:48:50 +0800

    howework02 is down

---
+++ GeekBandHomeWorkOC01/GeekBandHomeWorkOC01/IZFish.h
@@ -0,0 +1,17 @@
+//
+//  IZFish.h
+//  GeekBandHomeWorkOC01
+//
+//  Created by Liang Zisheng on 9/5/15.
+//  Copyright (c) 2015 ALMOND. All rights reserved.
+//
+
+#import "IZAnimal.h"
+
+@interface IZFish : IZAnimal
+
+@property (nonatomic, strong) NSString *color;
+
+- (void)swimming;
+
+@end
