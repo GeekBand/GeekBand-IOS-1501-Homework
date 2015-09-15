57f6f0429c3f4c1857eb1495d487e0d4d1a428c2
BlueMonkey <i_lzsh@163.com>
2015-09-15 22:50:02 +0800

    oc homework02_part1 is done

---
+++ G2015010284/GeekBandHomeworkOC02/GeekBandHomeworkOC02/IZChatTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  IZChatTableViewController.h
+//  GeekBandHomeworkOC02
+//
+//  Created by Liang Zisheng on 9/12/15.
+//  Copyright (c) 2015 ALMOND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IZChatTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *chatsList;
+
+@end
