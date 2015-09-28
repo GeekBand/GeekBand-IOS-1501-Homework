080917b364ade9d01197c402486fabd459cf5f9c
eapenzhan <guaiguaip@gamil.com>
2015-09-28 12:23:41 +0800

    ios开发作业1

---
+++ G2015010139/HomeWork_ios开发/HomeWork_ios开发/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeWork_ios开发
+//
+//  Created by john on 15/9/27.
+//  Copyright © 2015年 geekband-i150027. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ G2015010139/HomeWork_ios开发/HomeWork_ios开发/Message.m
@@ -0,0 +1,13 @@
+//
+//  Message.m
+//  HomeWork_ios开发
+//
+//  Created by john on 15/9/27.
+//  Copyright © 2015年 geekband-i150027. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@end
